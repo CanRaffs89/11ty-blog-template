d873f6f3a66a244740b65a9381ecaaa78683cce4
CanRaffs89 <24660563+CanRaffs89@users.noreply.github.com>
2023-07-23 21:40:53 +0100

    First commit

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+    
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    // Add a function to get the current year for the footer
+    eleventyConfig.addShortcode("currentYear", () => 
+        `${new Date().getFullYear()}`
+    );
+
+    // Add a filter to display nicer dates for posts
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    }
+};
